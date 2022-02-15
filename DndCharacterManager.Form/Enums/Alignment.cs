9e85c1e554157044cbe0c9d5854502b61d17bd2b
Brosbe <brosbe@protonmail.com>
2022-02-15 19:33:32 +0100

    Add alignment enum

---
+++ DndCharacterManager.Form/Enums/Alignment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DndCharacterManager.Form.Enums
+{
+    public enum Alignment
+    {
+        Lawful_Good,
+        Neutral_Good,
+        Chaotic_Good,
+        Lawful_Neutral,
+        True_Neutral,
+        Chaotic_Neutral,
+        Lawful_Evil,
+        Neutral_Evil,
+        Chaotic_Evils
+    }
+}
